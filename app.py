1f3b7ec6c6c0b0fa8f2f055e70d6654de7c86ea7
ADMIN <smallboyharish2004@gmail.com>
2022-12-28 14:16:46 +0530

    my first python program

---
+++ app.py
@@ -0,0 +1,6 @@
+name = str(input("Your name : "))
+b_year = int(input("Enter the birth year : "))
+cy_year = int(input("Enter the year from which to be calculated : "))
+age = cy_year - b_year
+age = str(age)
+print("Hi " + name + " Your age is :" + age)
